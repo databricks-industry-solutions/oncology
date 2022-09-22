ef7c165ae71489069f1cdb9f55682263771e6bc0
dbbnicole <86326159+dbbnicole@users.noreply.github.com>
2022-09-22 13:21:03 -0500

    Adding wait time for JSL cluster setup

--- RUNME.py
+++ RUNME.py
@@ -145,7 +145,7 @@ job_json = {
 
 dbutils.widgets.dropdown("run_job", "False", ["True", "False"])
 run_job = dbutils.widgets.get("run_job") == "True"
-nsc.deploy_compute(job_json, run_job=run_job)
+nsc.deploy_compute(job_json, run_job=run_job, wait=1800)
 
 # COMMAND ----------
 
